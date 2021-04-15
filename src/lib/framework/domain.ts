806cc71f186f3954f9137cdf9ad9137db0899dc6
wss-git <wssgryx@163.com>
2021-04-15 14:43:43 +0800

    feat: report && load component update

--- src/lib/framework/domain.ts
+++ src/lib/framework/domain.ts
@@ -105,7 +105,7 @@ export default class Component {
   }
 
   static async getAutoDomain(inputs: IInputs, serviceName: string, functionName: string): Promise<string> {
-    const domainComponent = await core.loadComponent('domain');
+    const domainComponent = await core.loadComponent('devsapp/domain');
 
     return await domainComponent.get({
       ...inputs,
